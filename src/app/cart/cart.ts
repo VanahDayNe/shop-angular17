8c9fb9ba86a31d264d4cafcfd6b36d2c24170c62
Vanah <160804927+VanahDayNe@users.noreply.github.com>
2024-11-14 23:48:25 +0700

    Delete src directory

--- src/app/cart/cart.ts
+++
@@ -1,8 +0,0 @@
-export interface Cart {
-    Id: number;
-    Name: string;
-    Price: number;
-    Quantity: number; //Số lượng
-    InStock: number; //Kho
-    ImageUrl: string;
-}
--- src/app/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-  Id: number;
-  Name: string;
-  Price: number;
-  ReleaseDate: string,
-  ImageUrl: string;
-  starRating: number;
-  InStock: number;
-}
